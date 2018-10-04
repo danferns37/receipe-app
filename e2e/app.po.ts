c649c084e31003ffafe8678125120dc23eaad7cf
feature/Daniel-Fernandes-QloyalCodeTest <danferns37@gmail.com>
2018-10-04 21:53:49 +1000

    Adding test cases for recipe

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,43 @@ export class RecipeAppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  manageadddeleterecipe(){
+    element(by.xpath("//h4[text()='Chinese Chicken']/ancestor::div[2]/div[1]")).click();
+    element(by.partialButtonText("Manage Recipes")).click();
+    element(by.partialButtonText("Add to shopping List")).click();
+   element(by.id("name")).sendKeys("Salad");
+   element(by.id("amount")).sendKeys("4");
+   element(by.buttonText("Add")).click();
+   element(by.xpath("//a[contains(text(),'Salad')]")).click();
+   element(by.buttonText("Delete")).click();
+ 
   }
+ 
+ 
+ 
+  manageclearrecipedetails(){
+   element(by.xpath("//h4[text()='Chinese Chicken']/ancestor::div[2]/div[1]")).click();
+    element(by.partialButtonText("Manage Recipes")).click();
+    element(by.partialButtonText("Add to shopping List")).click();
+   element(by.id("name")).sendKeys("Salad");
+   element(by.id("amount")).sendKeys("4");
+   element(by.buttonText("Clear")).click();
+ 
+ 
+ }
+ 
+  manageupdaterecipe(){
+   element(by.xpath("//h4[text()='Chinese Chicken']/ancestor::div[2]/div[1]")).click();
+   element(by.partialButtonText("Manage Recipes")).click();
+   element(by.partialButtonText("Add to shopping List")).click();
+   element(by.xpath("//a[contains(text(),'Chicken Portions')]")).click();
+   element(by.id("name")).clear();
+   element(by.id("name")).sendKeys("Chicken Portions_Half");
+   element(by.id("amount")).clear();
+   element(by.id("amount")).sendKeys("5");
+   element(by.buttonText("Update")).click();
+ 
+ 
+ }
+ 
 }
